d11d76d199fa47f945385a183f93ea66daa9d82f
JaidevSK <134712750+JaidevSK@users.noreply.github.com>
2025-01-09 11:01:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+""" This is for Pylint
+It has been updated multiple times
+"""
+
 A1 = 1
 B1 = 10
 
@@ -8,12 +12,14 @@ C = 1
 C = C + 1
 
 def a(a):
+    ""This is just for analysing the 
+    Working of Pylint"
+    print(a)
     for i in range(10):
-        a1 = 10
         b = 20
         d = a1+b
-        return d
-        print(d)
+    print(i)    
+    return d
 
 B2 = a(1)
 print(B2)

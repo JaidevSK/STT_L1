97854c3071e19a391827fd6d9f8cbc188d522ca8
JaidevSK <134712750+JaidevSK@users.noreply.github.com>
2025-01-09 10:53:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,26 @@
-a = 1
-b = 10
+A1 = 1
+B1 = 10
 
-for i in range (a, b):
-  print("Hello World")
+for i in range (A1, B1):
+	print("Hello World")
 
-c=1
-c = c+"a"
+C = 1
+C = C + 1
 
 def a(a):
-  for i in range(10):
-    a = 10
-    b = 20
-    d = a+b
-    return d
-    print(d)
+	for i in range(10):
+		a = 10
+		b = 20
+		d = a+b
+		return d
+		print(d)
 
 b = a(a)
 print(b)
 
-if a == b:
-  print(True)
-
+if (a == b):
+	print(True)	
 else: 
-  print(False)
+	print(False)
 
-print("Hello World")
-
-# This is a comment
-''' But is this?'''
-
+print("Hello World") # This is a comment

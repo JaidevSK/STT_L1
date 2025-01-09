36969023b770444cde6e3162d269a6a736096b0c
JaidevSK <134712750+JaidevSK@users.noreply.github.com>
2025-01-09 10:46:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,24 @@ c=1
 c = c+"a"
 
 def a(a):
-  return 1
+  for i in range(10):
+    a = 10
+    b = 20
+    d = a+b
+    return d
+    print(d)
 
 b = a(a)
-
 print(b)
+
+if a == b:
+  print(True)
+
+else: 
+  print(False)
+
+print("Hello World')
+
+# This is a comment
+''' But is this?"""
+
